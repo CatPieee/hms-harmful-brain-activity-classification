3d720809b0ce5286a2ec0e7209f1bda361e2f1e5
Ruiling <ruiling726@163.com>
2026-01-29 20:12:26 +0800

    revise to English version

--- test/kaggle_api_test.py
+++ test/kaggle_api_test.py
@@ -7,14 +7,14 @@ os.environ['KAGGLE_CONFIG_DIR'] = '/Users/ruiling/.kaggle'
 api = KaggleApi()
 try:
     api.authenticate()
-    print("✅ Kaggle API认证成功")
-    print("当前认证用户名:", api.get_config_value('username'))
-    print("当前认证密钥:", api.get_config_value('key'))
-    # print("用户:", api.config_value('username'))
+    print("✅ Kaggle API authentication successful")
+    print("Current authenticated username:", api.get_config_value('username'))
+    print("Current authenticated key:", api.get_config_value('key'))
+    # print("User:", api.config_value('username'))
 
     # 在刚才成功的脚本后面添加
     api.competition_download_files('hms-harmful-brain-activity-classification')
     print("下载指令已发送")
     
 except Exception as e:
-    print(f"❌ 认证失败: {str(e)}")
+    print(f"❌ Authentication failed: {str(e)}")
